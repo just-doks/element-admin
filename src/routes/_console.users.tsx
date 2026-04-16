73950942878c46706f75c8bfe3149afe2d43fcd2
just-doks <graviman13@gmail.com>
2026-04-16 12:46:11 +0300

    fix patternMismatch attempt 3

--- src/routes/_console.users.tsx
+++ src/routes/_console.users.tsx
@@ -413,8 +413,7 @@ const UserAddButton: React.FC<UserAddButtonProps> = ({
           <Form.TextControl
             onInput={onLocalpartInput}
             required
-            pattern="[-a-z0-9.=_/]+"
-            type="text"
+            pattern="[a-z0-9.=_\\/\\-]+"
             autoCapitalize="off"
             autoComplete="off"
           />
